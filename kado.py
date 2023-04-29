0270d68566d4c095654c26e7922056202b204eb7
firefood <65716578+foodwithfire@users.noreply.github.com>
2023-04-29 14:09:06 +0200

    Update kado.py

--- kado.py
+++ kado.py
@@ -1,12 +1,12 @@
-digits = "0123456789"
+DIGITS = "0123456789"
 
-integer = "INT"
-float = "FLOAT"
-plus = "PLUS"
-minus = "MIN"
-divide = "DIV"
-multiply = "MUL"
-endoffile = "EOF"
+INTEGER = "INT"
+FLOAT = "FLOAT"
+PLUS = "PLUS"
+MINUS = "MIN"
+DIVIDE = "DIV"
+MULTIPLY = "MUL"
+ENDOFFILE = "EOF"
 
 
 class Token:
@@ -15,50 +15,69 @@ class Token:
         self.type = type
         self.format = f"[{self.type}]"
         if self.value != None:
-            self.format += f" - '{self.value}'"
-        if self.type != endoffile:
+            self.format += f": '{self.value}'"
+        if self.type != ENDOFFILE:
             self.format += " | "
 
 
-class Decryptor:
+class Lexer:
     def __init__(self, user_input):
-        self.decrypted = []
+        self.tokens = []
         self.user_input = user_input
         self.current_char = ""
         self.pos = 0
-        self.decrypt()
+        self.transform_to_token()
         self.token_type = None
         self.token_value = None
 
-    def decrypt(self):
+    def transform_to_token(self):
+        error = False
+        self.current_char = self.user_input[self.pos]
         while self.pos < len(self.user_input):
-            self.current_char = self.user_input[self.pos]
-
-            if self.current_char in digits:
-                self.token_type = integer
-                self.token_value = self.current_char
+            if self.current_char in DIGITS:
+                self.number = self.current_char
+                while True:
+                    if self.current_char in DIGITS and self.next_char() == 1:
+                        self.number += self.current_char
+                    else:
+                        break
+                self.token_type = INTEGER
+                self.number_ = ""
+                for digit in self.number:
+                    if digit in DIGITS:
+                        self.number_ += digit
+                    else:
+                        self.pos -= 1
+                self.token_value = self.number_
+            elif self.current_char == ".":
+                self.token_type = FLOAT
             elif self.current_char == " ":
                 self.next_char()
                 continue
             elif self.current_char in "+-*/":
                 self.token_value = None
                 if self.current_char == "+":
-                    self.token_type = plus
+                    self.token_type = PLUS
                 elif self.current_char == "-":
-                    self.token_type = minus
+                    self.token_type = MINUS
                 elif self.current_char == "*":
-                    self.token_type = multiply
+                    self.token_type = MULTIPLY
                 elif self.current_char == "/":
-                    self.token_type = divide
+                    self.token_type = DIVIDE
             else:
-                self.decrypted = f"Error : Illegal character -> '{self.current_char}'"
+                self.tokens = f"Error : Illegal character -> '{self.current_char}'"
+                error = True
                 break
-            self.decrypted.append(Token(self.token_type, self.token_value).format)
+            self.tokens.append(Token(self.token_type, self.token_value).format)
             self.next_char()
 
-        if type(self.decrypted) == "<class 'list'>":
-            self.decrypted.append(Token(endoffile).format)
+        if not error:
+            self.tokens.append(Token(ENDOFFILE).format)
 
     def next_char(self):
         self.pos += 1
-        self.current_char = ""
+        try:
+            self.current_char = self.user_input[self.pos]
+            return 1
+        except:
+            return 0
