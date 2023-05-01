8645b796dbc3e4b7af6b96bf4c18011a6bb13147
firefood <DoukiYanis75@gmail.com>
2023-05-01 15:15:56 +0200

    update

--- shell.py
+++ shell.py
@@ -1,9 +1,10 @@
-import kado
+import kado, os
 
 while True:
 
     enter = input("Kado > ")
-    result, error = kado.run("<stdin>", enter)
+    if enter == "": continue
+    result, error = kado.run(os.path.basename(__file__), enter)
 
     if error:
         print(error.as_string())
